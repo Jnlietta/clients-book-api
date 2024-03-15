b7aa8cd44687789404e85635140e35825603e506
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-15 22:30:27 +0100

    Change a port for a server

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
