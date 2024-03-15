070ba26c6685927b285db145807a733043324d21
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-15 14:25:46 +0100

    Add middleware for links that not working

--- server.js
+++ server.js
@@ -60,6 +60,12 @@ app.delete('/testimonials/:id', (req, res) => {
     res.json(succes);
 });
 
+app.use((req, res) => {
+    const failure = { message: 'Not found...' };
+
+    res.status(404).json(failure);
+});
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
