a362989850cc4f627f27d9634d60f0fba2658472
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-15 22:29:28 +0100

    Add middleware with path to /client/build

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var cors = require('cors')
+const path = require('path');
 
 const app = express();
 
@@ -9,20 +10,22 @@ const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
 app.use(cors());
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', testimonialsRoutes); // add testimonials routes to server
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
-
 app.use((req, res) => {
     const failure = { message: 'Not found...' };
 
     res.status(404).json(failure);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.listen(8000, () => {
