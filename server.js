a5c0d155429a9e3a7de9fd8be64eca8d0be425b5
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-15 16:54:58 +0100

    Add files to client, install dependencies, add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var cors = require('cors')
 
 const app = express();
 
@@ -7,6 +8,7 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', testimonialsRoutes); // add testimonials routes to server
