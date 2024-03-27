af4ea375cbb0f259f35a1d5332d60f43337b1418
Julia Glinka <julia.w.glinka@gmail.com>
2024-03-27 22:39:15 +0100

    Modify endpoints and create controller for collection testimonialss

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,61 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
-const { v4: uuidv4 } = require('uuid');
+
+const TestimonialController = require('../controllers/testimonials.controller');
+
 
 // get all testimonials
-router.route('/testimonials').get((req, res) => {
-  res.json(db.testimonials);
-});
- 
-// get random client
-router.route('/testimonials/random').get((req, res) => {
-    const randomIndex = Math.floor(Math.random() * db.testimonials.length);
-    const randomClient = db.testimonials[randomIndex];
-  
-    res.json(randomClient);
-});
- 
-// get client by id
-router.route('/testimonials/:id').get((req, res) => {
-    const id = parseInt(req.params.id);    
-    const client = db.testimonials.find(client => client.id === id);
-  
-    res.json(client);
-});
-  
-// add client
-router.route('/testimonials').post((req, res) => {
-    const { author, text } = req.body;
-    const nextId = uuidv4();
-    const newClient = { id: nextId, author: author, text: text};
-    db.testimonials.push(newClient);
-    const succes = { message: 'OK' }
-  
-    res.json(succes);
-});
+router.get('/testimonials', TestimonialController.getAll);
 
-// make changes in clients data
-router.route('/testimonials/:id').put((req, res) => {
-    const id = parseInt(req.params.id);
-    const { author, text } = req.body;
-    const client = db.testimonials.find(client => client.id === id);
-    client.author = author;
-    client.text = text;
-    const succes = { message: 'OK' }
-  
-    res.json(succes);
-});
-  
-// delete client
-router.route('/testimonials/:id').delete((req, res) => {
-    const id = parseInt(req.params.id);
-    const client = db.testimonials.find(client => client.id === id);
-    const indexOfClient = db.testimonials.indexOf(client);
-    db.testimonials.splice(indexOfClient, 1);
-    const succes = { message: 'OK' }
-  
-    res.json(succes);
-});
+// get random testimonial
+router.get('/testimonials/random', TestimonialController.getRandom);
+
+// get testimonial by id
+router.get('/testimonials/:id', TestimonialController.getById);
+
+// add testimonial
+router.post('/testimonials', TestimonialController.postNew);
+
+// make changes in testimonials data
+router.put('/testimonials/:id', TestimonialController.putById);
+
+// delete testimonial
+router.delete('/testimonials/:id', TestimonialController.deleteById);
 
 module.exports = router;
