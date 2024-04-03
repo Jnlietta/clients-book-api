716fe8a9dae06920ad68d9e8c47e75084aa841de
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-03 18:18:12 +0200

    Add endpoints GET to search by performer, genre, price and day in concerts.routes

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -9,6 +9,18 @@ router.get('/concerts', ConcertController.getAll);
 // get concert by id
 router.get('/concerts/:id', ConcertController.getById);
 
+// get all concerts with the same performer
+router.get('/concerts/performer/:performer', ConcertController.getByPerformer);
+
+// get all concerts with the same genre
+router.get('/concerts/genre/:genre', ConcertController.getByGenre);
+
+// get all concerts with price between params price_min and price_max
+router.get('/concerts/price/:price_min/price_max', ConcertController.getByPrice);
+
+// get all concerts with the same day
+router.get('/concerts/day/:day', ConcertController.getByDay);
+
 // add concert
 router.post('/concerts', ConcertController.postNew);
 
